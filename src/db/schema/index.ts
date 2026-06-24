3a286ce61390c24bc973f6816a280b9e657ead0a
ajaySingh2615 <ajaysingh261526@gmail.com>
2026-06-24 13:14:07 +0530

    fix: correct relative import path in schema index

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,2 +1,2 @@
 // Re-export auth tables from their new home so existing imports don't break
-export { users, refreshTokens, authTokens } from "../modules/auth/user.model.js";
+export { users, refreshTokens, authTokens } from "../../modules/auth/user.model";
